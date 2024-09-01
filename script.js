725e7fc413ad0df6bbcb5218bfb06a33b7cc0d0e
brandon-parkerson <brandon-m-parkerson@gmail.com>
2024-09-01 15:06:59 +0300

    Display weather conditions to page.

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-
 const btn = document.querySelector("button");
-
-
 
 async function fetchWeather(location) {
   const response = await fetch(
@@ -9,12 +6,33 @@ async function fetchWeather(location) {
     { mode: "cors" }
   );
   const weather = await response.json();
-  console.log(weather.currentConditions);
+  console.log(weather);
+  const weatherInfo = info(
+    weather.resolvedAddress,
+    weather.currentConditions.temp,
+    weather.currentConditions.humidity,
+    weather.currentConditions.conditions,
+    weather.description
+  );
+  render(weatherInfo);
 }
 
 btn.addEventListener("click", async () => {
-    const input = document.querySelector("#search").value;
-    await fetchWeather(input);
-})
+  const input = document.querySelector("#search").value;
+  await fetchWeather(input);
+});
 
+function info(place, temp, humidity, conditions, description) {
+  return { place, temp, humidity, conditions, description };
+}
 
+function render(weatherInfo) {
+  const body = document.querySelector("body");
+  const card = document.createElement("div");
+  card.innerHTML = `<h2>${weatherInfo.place}</h2>
+  <p>${weatherInfo.temp}</p>
+  <p>${weatherInfo.humidity}</p>
+  <p>${weatherInfo.conditions}</p>
+  <p>${weatherInfo.description}</p>`;
+  body.appendChild(card);
+}
