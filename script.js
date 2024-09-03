55a888c7428e21eaa832c58ee32177b42add6d51
brandon-parkerson <brandon-m-parkerson@gmail.com>
2024-09-03 13:46:51 +0300

    Handle errors.

--- script.js
+++ script.js
@@ -3,22 +3,33 @@ const btn = document.querySelector("button");
 // async function that gets api
 async function fetchWeather(location) {
   // await keyword that waits for the async function promise to complete
-  const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=82JXSZ3PNPQLN546R6LCWJH22`,
-    { mode: "cors" }
-  );
-  const weather = await response.json(); // turns the data from api into json format
-  console.log(weather);
-  // Send wanted data to a function that returns an object of that data
-  const weatherInfo = info(
-    weather.resolvedAddress,
-    weather.currentConditions.temp,
-    weather.currentConditions.humidity,
-    weather.currentConditions.conditions,
-    weather.description
-  );
-  // Call render function to display data to the page
-  render(weatherInfo);
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=82JXSZ3PNPQLN546R6LCWJH22`,
+      { mode: "cors" }
+    );
+    const weather = await response.json(); // turns the data from api into json format
+
+    console.log(weather);
+    // Send wanted data to a function that returns an object of that data
+    const weatherInfo = info(
+      weather.resolvedAddress,
+      weather.currentConditions.temp,
+      weather.currentConditions.humidity,
+      weather.currentConditions.conditions,
+      weather.description
+    );
+
+    // Call render function to display data to the page
+    render(weatherInfo);
+  } catch {
+    const body = document.querySelector("body");
+    const card = document.createElement("div");
+    card.classList.add("data");
+    card.innerHTML = `<h2>Enter a valid location</h2>`
+    body.appendChild(card);
+    console.error("Failed");
+  }
 }
 
 btn.addEventListener("click", async () => {
