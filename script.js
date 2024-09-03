1c55fdcd59d5cdf831867198dfa9dd1be07283e5
brandon-parkerson <brandon-m-parkerson@gmail.com>
2024-09-03 13:16:14 +0300

    Re-render data when new city is typed in.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const btn = document.querySelector("button");
 
+// async function that gets api
 async function fetchWeather(location) {
+  // await keyword that waits for the async function promise to complete
   const response = await fetch(
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=82JXSZ3PNPQLN546R6LCWJH22`,
     { mode: "cors" }
   );
-  const weather = await response.json();
+  const weather = await response.json(); // turns the data from api into json format
   console.log(weather);
+  // Send wanted data to a function that returns an object of that data
   const weatherInfo = info(
     weather.resolvedAddress,
     weather.currentConditions.temp,
@@ -14,14 +17,18 @@ async function fetchWeather(location) {
     weather.currentConditions.conditions,
     weather.description
   );
+  // Call render function to display data to the page
   render(weatherInfo);
 }
 
 btn.addEventListener("click", async () => {
   const input = document.querySelector("#search").value;
+  clearRender();
+
   await fetchWeather(input);
 });
 
+// Turn data to object
 function info(place, temp, humidity, conditions, description) {
   return { place, temp, humidity, conditions, description };
 }
@@ -29,10 +36,21 @@ function info(place, temp, humidity, conditions, description) {
 function render(weatherInfo) {
   const body = document.querySelector("body");
   const card = document.createElement("div");
+  card.classList.add("data");
   card.innerHTML = `<h2>${weatherInfo.place}</h2>
-  <p>${weatherInfo.temp}</p>
-  <p>${weatherInfo.humidity}</p>
+  <p>Temperature: ${weatherInfo.temp} F</p>
+  <p>Humidity: ${weatherInfo.humidity}%</p>
   <p>${weatherInfo.conditions}</p>
   <p>${weatherInfo.description}</p>`;
   body.appendChild(card);
+}
+
+function clearRender() {
+  const card = document.querySelector(".data");
+  // If card has data then remove it
+  if (card !== null) {
+    card.remove();
+  } else {
+    return;
+  }
 }
